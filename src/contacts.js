0bac41250cddafdb55ac116cabf36ee640c9f09c
JavarBS <154936558+JavarBS@users.noreply.github.com>
2024-10-16 21:57:16 -0700

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Javar",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT92wFTKAT4eTZ7GJaYOO78tLgMpTBhtTk-3A&s",
+    phone: "+304 677 341",
+    email: "scott.javar@yahoo.com",
+  },
+  {
+    name: "Odell",
+    imgURL:
+      "https://www.denverpost.com/wp-content/uploads/2016/10/odell-beckham.jpg?w=525",
+    phone: "+987 654 321",
+    email: "O@odell.com",
+  },
+  {
+    name: "MJ",
+    imgURL:
+      "https://m.media-amazon.com/images/M/MV5BMTM1NjExNjg1OV5BMl5BanBnXkFtZTcwMTQ0NzIwMw@@._V1_FMjpg_UX1000_.jpg",
+    phone: "+918 372 574",
+    email: "M@jackson.com",
+  },
+];
+
+export default contacts;
